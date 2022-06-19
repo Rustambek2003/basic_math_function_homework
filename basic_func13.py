e7a5dcf693aa5793b659a56d8eba7707764271c0
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-06-19 09:00:27 +0500

    13

--- basic_func13.py
+++ basic_func13.py
@@ -1,3 +1,9 @@
 # Create a function called main.
 # Create function argument a.
 # Returns the square root of a number.
+from math import sqrt
+def main():
+    a = 9
+    a = sqrt(a)
+    print(a)
+main()
