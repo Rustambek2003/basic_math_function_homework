36caa030a616cf1d0283c91bc0373d595c48621a
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-06-19 09:44:10 +0500

    11

--- basic_func11.py
+++ basic_func11.py
@@ -1,8 +1,8 @@
 # Create a function called main.
 # Create function argument a.
 # Return the absolute value of a.
-def main():
-    a = -8
-    a = abs(a)
-    print(a)
-main()
+from math import floor
+def main(a):
+    return a
+print(main(8))
+
