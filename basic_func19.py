73e74050ab19bc37f38fee82911124ca9aa7eee9
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-06-19 09:23:04 +0500

    19

--- basic_func19.py
+++ basic_func19.py
@@ -1,3 +1,8 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the absolute value of the difference between a and b.
+def main():
+    a = 4
+    b = 11
+    print(abs(a - b))
+main()
