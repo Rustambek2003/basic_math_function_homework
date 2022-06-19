012cf2f2e2379b2cefc9d4097d22100a27c85fd8
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-06-19 09:47:20 +0500

    12

--- basic_func12.py
+++ basic_func12.py
@@ -2,8 +2,6 @@
 # Create function argument a.
 # Return the value of a.
 # Round the result to 2 decimal places.
-def main():
-    a = 6.453
-    a = round(a, 2)
-    print(a)
-main()
+def main(a):
+    return round(a, 2)
+print(main(6.453))
